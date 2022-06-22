1c6f356de2138e093f1a029c3ae3c8b1fcc5d9e7
ghavasi <g.havasi@outlook.com>
2022-06-22 22:31:25 +0100

    database settings changed

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  user: "akzqousrvffswq",
+  host: "ec2-34-242-8-97.eu-west-1.compute.amazonaws.com",
+  database: "d8ch0id1b29srl",
+  password: "deae82e2628f075ea26660eb3a140ed83527fb02e8713411368fccd37f9bc926",
+  port: "5432",
   //ssl: { rejectUnauthorized: false },
 });
 
