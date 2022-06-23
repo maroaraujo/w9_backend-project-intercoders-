a84e0556e80acc956c6795c8b1b5955f8b6c227b
maroaraujo <tiagomoller@tiagos-mbp.home>
2022-06-23 10:40:10 +0100

    trying to fix authorization token

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ import announcementRouter from "./routes/announcement.js";
 
 //unpack json
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 
 app.use("/users", userRouter);
 app.use("/waitinglist", waitingListRouter);
